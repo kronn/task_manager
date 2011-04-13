afb41f5d8706d71395b32a81b10b0b664eab8a7e
Matthias Viehweger <kronn@kronn.de>
2011-04-13 14:05:14 +0200

    add summary and description

--- lib/task_manager/version.rb
+++ lib/task_manager/version.rb
@@ -4,7 +4,11 @@ unless defined?(TaskManager::VERSION)
   module TaskManager
     VERSION = "0.0.1"
     DATE = File.mtime(__FILE__)
-    SUMMARY = ''
-    DESCRIPTION = ''
+    SUMMARY = 'A simple wrapper around the Rufus::Scheduler to have a more configurable setup.'
+    DESCRIPTION = <<-EOT
+      A simple wrapper around the Rufus::Scheduler to have a more configurable setup.
+
+      The Task to be scheduled should be defined in a subclass.
+    EOT
   end
 end
