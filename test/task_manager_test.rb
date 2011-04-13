8baa70a9436e0455531424c75fb3913bd170bc75
Matthias Viehweger <kronn@kronn.de>
2011-04-13 13:50:45 +0200

    write test for the obvious

---
+++ test/task_manager_test.rb
@@ -0,0 +1,46 @@
+require 'rubygems'
+
+require 'more_unit_test/assert_stdout'
+require 'test/unit'
+
+require 'task_manager'
+
+class TaskManagerTest < Test::Unit::TestCase
+  # remember to clean up after each test-run
+  def teardown
+    # @sut.unschedule(:internal)
+    @sut = nil
+  end
+
+  # lets start by trying to create a taskmanager
+  def test_creating_a_blank_taskmanager
+    assert_nothing_raised do
+      TaskManager.new
+      TaskManager.new('staging', '/path/to/project')
+    end
+  end
+
+  # now that thats out of the way, define some helper methods
+  def tm
+    @sut ||= TaskManager.new
+  end
+
+  # assert_stdout_block does Expected==Actual internally, therefore I give a more flexbile expectation
+  class MatchableString < String
+    def initialize(string)
+      @data = string
+    end
+
+    def ==(other)
+      /#{Regexp.escape(@data)}/.match(other)
+    end
+  end
+
+  # on to the show
+
+  def test_taskmanager_can_output_messages
+    assert_stdout_block MatchableString.new('testing') do
+      TaskManager.new.say('testing')
+    end
+  end
+end
