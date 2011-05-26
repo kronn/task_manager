9fc18b8d25ede09a1e87f32b10d8bd23828dbc70
Matthias Viehweger <kronn@kronn.de>
2011-05-26 15:32:49 +0200

    be more confident about defining constants

--- lib/task_manager/version.rb
+++ lib/task_manager/version.rb
@@ -1,14 +1,12 @@
 # vim:ft=ruby:fileencoding=utf-8
 
-unless defined?(TaskManager::VERSION)
-  module TaskManager
-    VERSION = "0.0.2"
-    DATE = File.mtime(__FILE__)
-    SUMMARY = 'A simple wrapper around the Rufus::Scheduler to have a more configurable setup.'
-    DESCRIPTION = <<-EOT
-      A simple wrapper around the Rufus::Scheduler to have a more configurable setup.
+module TaskManager
+  VERSION = "0.0.2"
+  DATE = File.mtime(__FILE__)
+  SUMMARY = 'A simple wrapper around the Rufus::Scheduler to have a more configurable setup.'
+  DESCRIPTION = <<-EOT
+    A simple wrapper around the Rufus::Scheduler to have a more configurable setup.
 
-      The Task to be scheduled should be defined in a subclass.
-    EOT
-  end
+    The Task to be scheduled should be defined in a subclass.
+  EOT
 end
