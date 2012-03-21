f579c7132383096a279323e57da58e93bad1331d
Matthias Viehweger <kronn@kronn.de>
2012-03-21 11:11:31 +0100

    adding a patch for the replace output-buffer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,18 @@ require 'more_unit_test/assert_stdout'
 require 'test/unit'
 require 'mocha'
 
+class Test::Unit::Catch_IO
+  def sync=(value)
+    value
+  end
+
+  def puts(string)
+    write("#{string}\n")
+  end
+
+  def print(string)
+    write(string)
+  end
+end
+
 require 'task_manager'
